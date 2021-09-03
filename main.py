1958776a09a3e8365edb912b434b3e0abd1aabdc
Onigawa <eric.hantala@wanadoo.fr>
2021-09-03 14:28:01 +0200

    Working comments extraction

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import yaml
 import requests
 import pandas as pd
+import praw
+import json
+from urllib.request import urlopen
 
 config = yaml.safe_load(open("config.yaml", 'r'))
 
 CLIENT_ID = config.get("client_id")
 SECRET_KEY = config.get("secret_key")
-auth = requests.auth.HTTPBasicAuth(CLIENT_ID,SECRET_KEY)
+auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
 
 # here we pass our login method (password), username, and password
 data = {'grant_type': 'password',
@@ -29,23 +32,48 @@ headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
 # while the token is valid (~2 hours) we just add headers=headers to our requests
 requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
 
-res = requests.get("https://oauth.reddit.com/r/magicTCG/new",
-                   headers=headers)
+def get_comment_posts(post_id):
+    comments = json.loads(urlopen("https://www.reddit.com/comments/"+post_id+".json").read())
+    post = comments[0]["data"]["children"][0]
+    df = pd.DataFrame()  # initialize dataframe
+    for comment in comments[1]["data"]["children"]:
+        print(comment['data']['body'])
+        df = df.append({
+            'subreddit': post['data']['subreddit'],
+            'title': post['data']['title'],
+            'selftext': post['data']['selftext'],
+            'url': post['data']['url'],
+            'id': post['data']['id'],
+            'upvote_ratio': post['data']['upvote_ratio'],
+            'body': comment['data']['body']
 
-df = pd.DataFrame()  # initialize dataframe
+        }, ignore_index=True)
 
-# loop through each post retrieved from GET request
-for post in res.json()['data']['children']:
-    # append relevant data to dataframe
-    df = df.append({
-        'subreddit': post['data']['subreddit'],
-        'title': post['data']['title'],
-        'selftext': post['data']['selftext'],
-        'url': post['data']['url'],
-        'upvote_ratio': post['data']['upvote_ratio'],
-        'ups': post['data']['ups'],
-        'downs': post['data']['downs'],
-        'score': post['data']['score']
-    }, ignore_index=True)
+    return(df)
 
-df.to_csv("results.csv",sep=";")
+# params mode = new hot or top
+def get_new_posts(subreddit="magicTCG", mode="new"):
+    request_results = requests.get("https://oauth.reddit.com/r/" + subreddit + "/" + mode + "",
+                                   headers=headers)
+
+    df = pd.DataFrame()  # initialize dataframe
+
+    # loop through each post retrieved from GET request
+    for post in request_results.json()['data']['children']:
+        # append relevant data to dataframe
+        df = df.append({
+            'subreddit': post['data']['subreddit'],
+            'title': post['data']['title'],
+            'selftext': post['data']['selftext'],
+            'url': post['data']['url'],
+            'id':post['data']['id'],
+            'upvote_ratio': post['data']['upvote_ratio'],
+            'ups': post['data']['ups'],
+            'downs': post['data']['downs'],
+            'score': post['data']['score']
+        }, ignore_index=True)
+    return df
+
+
+results = get_new_posts()
+results.to_csv("results.csv", sep=";", encoding="UTF-8")
